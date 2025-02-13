3d40ef4fe99f935e460f7b2b1cea1643e80b722a
kimsungmo0108 <rlatjdah8585@daum.net>
2025-02-13 21:29:32 +0900

    init: Spring Boot 기본 설정 및 프로젝트 생성
    Spring Boot 기본 설정 및 프로젝트 생성

---
+++ moviepicks/src/main/java/com/moviepicks/MoviepicksApplication.java
@@ -0,0 +1,13 @@
+package com.moviepicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepicksApplication.class, args);
+	}
+
+}
---
+++ moviepicks/src/test/java/com/moviepicks/MoviepicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviepicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviepicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
