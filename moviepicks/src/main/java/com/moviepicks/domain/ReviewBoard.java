d5a3e66f354ecd24df024b42516ae30cbb1f3c45
sungchan98 <jvnhkdm0807@gmail.com>
2025-02-25 17:07:45 +0900

    feature-create-review-board

---
+++ moviepicks/src/main/java/com/moviepicks/domain/ReviewBoard.java
@@ -0,0 +1,44 @@
+package com.moviepicks.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "review_board")
+public class ReviewBoard {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "review_board_id", nullable = false, updatable = false)
+  private Long reviewBoardId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "point", nullable = false)
+  private int point;
+
+  @Column(name = "board_create_date", nullable = false, updatable = false)
+  private LocalDateTime boardCreateDate;
+
+  public ReviewBoard() {
+    this.boardCreateDate = LocalDateTime.now();
+  }
+}
